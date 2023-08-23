95e62205fdf433c463bb00174e1acf08e583244f
Luiz Vinícius <viniciusadr244@gmail.com>
2023-08-23 08:33:16 -0300

    add fila

---
+++ src/Fila.java
@@ -0,0 +1,24 @@
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public void enfileira(T elem) {
+        super.adiciona(elem);
+    }
+
+    public T espiar() {
+        if (this.isVazia()) {
+            return null;
+        }
+        return this.vetor[0];
+    }
+    
+    public T desenfileira() {
+        if (this.isVazia()) return null;
+        
+        T first = this.vetor[0];
+        super.remove(0);
+        return first;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        var quociente = new Pilha<Integer>(10);
-        
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.print("Digite o número: ");
-            int num = scan.nextInt();
-            while (num > 0) {
-                quociente.empilha(num % 2);
-                num /= 2;
-            }
-            while (!quociente.isVazia()) {
-                System.out.print(quociente.desempilha());
-            }
-            
-            
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        var fila = new Fila<Integer>(10);
+
+        fila.enfileira(2);
+        fila.enfileira(3);
+        fila.enfileira(4);
+        fila.enfileira(5);
+
+        fila.desenfileira();
+
+        System.out.println(fila);
     }
 }
