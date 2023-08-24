d0c979e48723475eb41c1a66f289f60631b16cdd
Luiz Vinícius <viniciusadr244@gmail.com>
2023-08-24 09:47:00 -0300

    ignore

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         var fila = new Fila<Integer>(10);
-
-        fila.enfileira(2);
-        fila.enfileira(3);
-        fila.enfileira(4);
-        fila.enfileira(5);
-
-        fila.desenfileira();
 
         System.out.println(fila);
     }
