7b3b1d9c9e3730645a150f9baa2589478566727c
Luiz Vinicius <viniciusadr244@gmail.com>
2024-04-18 20:23:41 -0300

    add

--- src/com/luiz/datastructures/lista/ListaTeste.java
+++ src/com/luiz/datastructures/lista/ListaTeste.java
@@ -3,12 +3,19 @@ package com.luiz.datastructures.lista;
 public class ListaTeste {
     public static void main(String[] args) {
         var lista = new ListaEncadeada<Integer>();
-
-        lista.adiciona(6);
-        lista.adiciona(9);
-        lista.adiciona(10);
-        lista.adiciona(1, 7);
-
+        
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
+        lista.adiciona(4);
+        // [1, 2, 3, 4, 5]
+        lista.adiciona(4, 5);
+       
+        System.out.println(lista.removeIndex(0));
+        System.out.println(lista);
+        System.out.println(lista.removeIndex(2));
+        System.out.println(lista);
+        System.out.println(lista.removeIndex(1));
         System.out.println(lista);
     }
 }
