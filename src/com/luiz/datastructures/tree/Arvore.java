241c7983da21a6a3bd49b13d82f1c08deaa386cb
Luiz Vinicius <viniciusadr244@gmail.com>
2024-04-27 10:15:59 -0300

    feature: em Nivel, height, maior, menor

--- src/com/luiz/datastructures/tree/Arvore.java
+++ src/com/luiz/datastructures/tree/Arvore.java
@@ -1,5 +1,7 @@
 package com.luiz.datastructures.tree;
 
+import com.luiz.datastructures.fila.Fila;
+
 public class Arvore<T> {
     private No<T> raiz;
     private int tamanho;
@@ -48,24 +50,14 @@ public class Arvore<T> {
         No<T> atual = this.raiz;
         @SuppressWarnings("unchecked")
         Comparable<T> chave = (Comparable<T>) valor;
-        int index = 0;
-        while (index < this.tamanho) {
+        while (atual != null) {
             if (chave.compareTo(atual.getValor()) < 0) {
-                if (atual.getEsquerda() != null) {
-                    atual = atual.getEsquerda();
-                } else {
-                    return false;
-                }
+                atual = atual.getEsquerda();
             } else if (chave.compareTo(atual.getValor()) > 0) {
-                if (atual.getDireita() != null) {
-                    atual = atual.getDireita();
-                } else {
-                    return false;
-                }
+                atual = atual.getDireita();
             } else {
                 return true;
             }
-            index++;
         }
         return false;
     }
@@ -99,6 +91,65 @@ public class Arvore<T> {
             posOrdem(init.getDireita());
             System.out.print(init.getValor() + " ");
         }
+    }
+
+    public void emNivel() {
+        if (this.raiz == null) {
+            return;
+        }
+
+        var fila = new Fila<No<T>>(this.tamanho);
+        fila.enfileira(this.raiz);
+
+        System.out.print("[");
+        while (fila.tamanho() != 0) {
+            No<T> removido = fila.desenfileira();
+            if (removido.getEsquerda() != null) {
+                fila.enfileira(removido.getEsquerda());
+            }
+            if (removido.getDireita() != null) {
+                fila.enfileira(removido.getDireita());
+            }
+            System.out.print(" " + removido);
+        }
+        System.out.print("]");
+    }
+
+    public int height(No<T> init) { // usa a lógica do posOrdem | Entender melhor
+        if (init == null) {
+            return 0;
+        }
+        if (init.getEsquerda() == null && init.getDireita() == null) {
+            return 1;
+        }
+
+        int altEsq = 0, altDir = 0;
+
+        if (init.getEsquerda() != null) {
+            altEsq = this.height(init.getEsquerda());
+        }
+
+        if (init.getDireita() != null) {
+            altDir = this.height(init.getDireita());
+        }
+
+        return 1 + Math.max(altEsq, altDir);
+    }
+
+    public T menor() {
+        No<T> atual = this.raiz;
+        while (atual.getEsquerda() != null) {
+            atual = atual.getEsquerda();
+        }
+        return atual.getValor();
+    }
+
+    public T maior() {
+        No<T> atual = this.raiz;
+        while (atual.getDireita() != null) {
+            atual = atual.getDireita();
+        }
+        return atual.getValor();
     }
 
     public int getTamanho() {
