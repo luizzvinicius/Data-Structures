e2225dc3259f9db896567382c1c961c314d7542c
Luiz Vinicius <viniciusadr244@gmail.com>
2024-05-01 18:53:33 -0300

    Selection ok

---
+++ src/com/luiz/sorting_algorithms/Selection.java
@@ -0,0 +1,32 @@
+package com.luiz.sorting_algorithms;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        var exemplo = new int[] { 4, 9, 2, 1, 7, 8, 0, 3, 15, 11 };
+        var representacao = Arrays.toString(selection(exemplo));
+
+        System.out.println(representacao);
+
+    }
+
+    public static int[] selection(int[] desordenada) {
+        int indiceMenor = 0, copia;
+        for (int i = 0; i < desordenada.length - 1; i++) {
+            indiceMenor = i;
+            for (int ii = i; ii < desordenada.length; ii++) { // ao final do for anterior, diminui o tamanho que percorre
+                if (desordenada[ii] < desordenada[indiceMenor]) {
+                    indiceMenor = ii;
+                }
+
+                if (desordenada[i] > desordenada[ii]) {
+                    copia = desordenada[ii];
+                    desordenada[ii] = desordenada[i];
+                    desordenada[i] = copia;
+                }
+            }
+        }
+        return desordenada;
+    }
+}
