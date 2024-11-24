4724ba315eac741aaecca41bef0105b68ecd04a7
Luiz Vinicius <viniciusadr244@gmail.com>
2024-11-24 10:40:19 -0300

    feat: simplify QuickSort

--- src/com/luiz/sorting_algorithms/QuickSort.java
+++ src/com/luiz/sorting_algorithms/QuickSort.java
@@ -4,35 +4,36 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        var exemplo = new int[] { 4, 7, 2, 6, 4, 1, 8, 3 };
+        var exemplo = new int[]{4, 7, 0, 2, 6, 4, 9, 1, 8, 3};
 
         quickSort(exemplo);
         System.out.println(Arrays.toString(exemplo));
     }
 
-    public static void quickSort(int[] array, int inicio, int fim) {
-        if (inicio >= fim) {
-            return;
-        }
+    public static void quickSort(int[] array, int start, int end) {
+        if (end <= start) return;
 
-        int pivo = array[fim], menorPivo = inicio, maiorPivo = fim;
-
-        while (menorPivo < maiorPivo) {
-            while (array[menorPivo] <= pivo && menorPivo < maiorPivo) {
-                menorPivo++;
-            }
-            while (array[maiorPivo] >= pivo && menorPivo < maiorPivo) {
-                maiorPivo--;
-            }
-            troca(array, menorPivo, maiorPivo);
-        }
-
-        troca(array, menorPivo, fim);
-        quickSort(array, inicio, menorPivo - 1);
-        quickSort(array, maiorPivo + 1, fim);
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
     }
 
-    private static void troca(int[] array, int index1, int index2) {
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end - 1; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        i++;
+        swap(array, i, end);
+        return i;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
         int aux = array[index1];
         array[index1] = array[index2];
         array[index2] = aux;
